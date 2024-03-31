72df21747bb10b84e37f04af05391170409b0cb2
amber1405 <ambercs1405@gmail.com>
2024-03-31 18:21:09 +0530

    Database configuration , Added utility functions and configured express, cookieParser and cors

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import connectDB from "./config/database.js";
+const PORT = process.env.PORT || 8000;
+import { app } from "./app.js";
+dotenv.config({ path: "./.env" });
+
+connectDB()
+  .then(() => {
+    app.listen(PORT || 8000, () => {
+      console.log(`Server is running at port : ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+  });
